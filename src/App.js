d26393588d108e1911f058e95bdbb412435a3f29
julia Wager <julia@p-wagner.at>
2024-04-22 20:21:39 +0200

    feat: create and style sign up form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container"
 import {Route, Routes} from "react-router-dom";
+import "./api/axiosDefaults"
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
                 <Route exact path="/" element={<h1>Home page</h1>}/>
                 <Route exact path="/feed" element={<h1>Feed</h1>}/>
                 <Route path="/signin" element={<h1>Sign in</h1>}/>
-                <Route path="/signup" element={<h1>Sign up</h1>}/>
+                <Route path="/signup" element={<SignUpForm/>}/>
                 <Route path="*" element={<p>Page not found!</p>}/>
             </Routes>
         </Container>
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,125 @@
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import playground from "../../assets/playground.svg"
+
+import {Form, Button, Image, Col, Row, Container, Alert} from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    });
+    const { username, password1, password2 } = signUpData;
+
+    const [errors, setErrors] = useState({});
+
+    const navigate= useNavigate();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/registration/", signUpData)
+            navigate("/signin");
+        } catch (err) {
+            setErrors(err.response?.data)
+        }
+    }
+
+    return (
+        <Container className={styles.Container}>
+            <Row className={`my-auto ${styles.Row}`}>
+                <Col className="my-auto py-2 p-md-2 mx-auto" md={8}>
+                    <Container className={`${appStyles.Content} p-4 `}>
+                        <h1 className={styles.Header}>Create your account</h1>
+
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="username">
+                                <Form.Label className="d-none">Username</Form.Label>
+                                <Form.Control
+                                    className={styles.Input}
+                                    type="text"
+                                    placeholder="Username"
+                                    name="username"
+                                    value={username}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            {errors.username?.map((message, idx) =>
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            )}
+
+                            <Form.Group className="mb-3" controlId="password1">
+                                <Form.Label className="d-none">Password</Form.Label>
+                                <Form.Control
+                                    className={styles.Input}
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password1"
+                                    value={password1}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            {errors.password1?.map((message, idx) =>
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            )}
+
+                            <Form.Group className="mb-3" controlId="password2">
+                                <Form.Label className="d-none">Confirm password</Form.Label>
+                                <Form.Control
+                                    className={styles.Input}
+                                    type="password"
+                                    placeholder="Confirm password"
+                                    name="password2"
+                                    value={password2}
+                                    onChange={handleChange}
+                                />
+                            </Form.Group>
+                            {errors.password2?.map((message, idx) =>
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            )}
+                            {errors.non_field_errors?.map((message, idx) =>
+                                <Alert variant="warning" className="mt-3" key={idx}>{message}</Alert>
+                            )}
+
+                            <Button className={`${btnStyles.Button} ${btnStyles.Wide}`} type="submit">
+                                Sign up
+                            </Button>
+                        </Form>
+
+                    </Container>
+                    <Container className={`mt-3 ${appStyles.Content}`}>
+                        <Link className={styles.Link} to="/signin">
+                            Already have an account? <span>Sign in</span>
+                        </Link>
+                    </Container>
+                </Col>
+            </Row>
+            <Row className={styles.Row}>
+                <Col
+                    md={12}
+                    className={`my-auto p-2`}
+                >
+                    <Image
+                        className={`${appStyles.FillerImage}`}
+                        src={playground}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default SignUpForm;
