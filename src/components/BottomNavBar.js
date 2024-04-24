51f8b09ecac60d55b23c8ba61f4169807ea244d7
julia Wager <julia@p-wagner.at>
2024-04-24 22:01:25 +0200

    feat: add correct navlinks to bottom nav bar

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -2,16 +2,32 @@ import React from 'react'
 import {Container, Nav, Navbar} from "react-bootstrap";
 import styles from "../styles/NavBar.module.css"
 import { NavLink } from "react-router-dom";
+import {useCurrentUser} from "../contexts/CurrentUserContext";
 
 const BottomNavBar = () => {
+    const currentUser = useCurrentUser();
+
+    const loggedInIcons = <>
+        <NavLink to="/feed" id={styles.feed} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Feed</NavLink>
+        <NavLink to="/following" id={styles.following} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Following</NavLink>
+        <NavLink to="/matches" id={styles.matches} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Matches</NavLink>
+        <NavLink to="/profile" id={styles.profile} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Profile</NavLink>
+    </>
+
+    const loggedOutIcons = (
+        <>
+            <NavLink to="/feed" id={styles.feed} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Feed</NavLink>
+            <NavLink to="/signin" id={styles.sign} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Sign in</NavLink>
+            <NavLink to="/signup" id={styles.sign} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Sign up</NavLink>
+        </>
+    )
+
     return (
         <Navbar className={`d-md-none ${styles.BottomNavBar}`} sticky="bottom">
             <Container className="p-0">
                 <Navbar.Collapse id="bottom-navbar-nav">
                     <Nav className={`text-center w-100 ${styles.BottomGrid}`}>
-                        <NavLink to="/feed" id={styles.feed} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Feed</NavLink>
-                        <NavLink to="/signin" id={styles.sign} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Sign in</NavLink>
-                        <NavLink to="/signup" id={styles.sign} className={({ isActive }) => isActive ? styles.BottomActive : styles.BottomNavLink}>Sign up</NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
