5da78ffdeac4f27cdaa701a623e8d4fb99d22c45
julia Wager <julia@p-wagner.at>
2024-04-28 19:54:26 +0200

    feat: add custom hook to toggle burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,13 @@ import styles from "../styles/NavBar.module.css"
 import { NavLink } from "react-router-dom";
 import {useCurrentUser, useSetCurrentUser} from "../contexts/CurrentUserContext";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -32,14 +35,17 @@ const NavBar = () => {
     )
 
     return (
-        <Navbar className={styles.NavBar} expand="md" sticky="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" sticky="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
                         <img className={styles.Logo} src={logo} alt="logo" height="45" />
                     </Navbar.Brand>
                 </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle
+                    ref={ref}
+                    onClick={() => setExpanded(!expanded)}
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto text-center">
                         {currentUser ? loggedInIcons : loggedOutIcons}
