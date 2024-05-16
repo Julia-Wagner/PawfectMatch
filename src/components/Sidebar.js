66e1936bc352fa20525ac3dd20f764fe7a265264
julia Wager <julia@p-wagner.at>
2024-05-16 18:03:41 +0200

    feat: show information for unauthorized users in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,7 +19,7 @@ const Sidebar = () => {
     useEffect(() => {
         const fetchSavedPosts = async () => {
             try {
-                const {data} = await axiosReq.get("/saves")
+                const {data} = await axiosReq.get("/saves");
                 const postIds = data.results.map((save) => save.post).slice(0, 5);
                 const postItems = await Promise.all(postIds.map(async (postId) => {
                     const { data: post } = await axiosReq.get(`/posts/${postId}`);
@@ -40,25 +40,31 @@ const Sidebar = () => {
         <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
             <Container className={`p-4 ${appStyles.Content}`}>
                 <h3 className={`text-center ${appStyles.SidebarHeading}`}>Saved Posts</h3>
-                {hasLoaded ? (
+                {currentUser ? (
                     <>
-                        <div className="mt-4 text-left">
-                            {posts.length ? (
-                                posts.map((post) => (
-                                    <p key={post.id}>
-                                        <Link to={`/posts/${post.id}`} className={appStyles.SidebarLink}>{post.title} </Link>
-                                        by {post.owner}</p>
-                                ))
-                                ) : (
-                                <p>You have no saved posts yet.</p>
-                            )}
-                            <div className="mt-4 text-center">
-                                <Link to={"/saves"}>Show all saved posts</Link>
-                            </div>
-                        </div>
+                        {hasLoaded ? (
+                            <>
+                                <div className="mt-4 text-left">
+                                    {posts.length ? (
+                                        posts.map((post) => (
+                                            <p key={post.id}>
+                                                <Link to={`/posts/${post.id}`} className={appStyles.SidebarLink}>{post.title} </Link>
+                                                by {post.owner}</p>
+                                        ))
+                                    ) : (
+                                        <p>You have no saved posts yet.</p>
+                                    )}
+                                    <div className="mt-4 text-center">
+                                        <Link to={"/saves"}>Show all saved posts</Link>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <Asset spinner />
+                        )}
                     </>
                 ) : (
-                    <Asset spinner />
+                    <p>Please sign in to see your saved posts.</p>
                 )}
             </Container>
         </Col>
