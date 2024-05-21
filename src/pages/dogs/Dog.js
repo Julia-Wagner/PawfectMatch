c6b37c22ac82064cd296fb1370eb475288d5ee21
julia Wager <julia@p-wagner.at>
2024-05-21 21:51:12 +0200

    feat: show dog details and characteristics on detail page

--- src/pages/dogs/Dog.js
+++ src/pages/dogs/Dog.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react"
 import DOMPurify from 'dompurify';
 import styles from "../../styles/Post.module.css"
-import {Badge, Card, Image} from "react-bootstrap";
+import {Badge, Card, Col, Container, Image, Row} from "react-bootstrap";
 import {Link, useNavigate} from "react-router-dom";
 import {MoreDropdown} from "../../components/MoreDropdown";
 import {axiosRes} from "../../api/axiosDefaults";
@@ -13,6 +13,7 @@ const Dog = (props) => {
         owner,
         is_owner,
         birthday,
+        age,
         breed,
         characteristics,
         gender,
@@ -98,6 +99,33 @@ const Dog = (props) => {
                     dangerouslySetInnerHTML={sanitizedDescription}
                 />}
                 {!dogPage && <Card.Link href={`/dogs/${id}`}>View dog</Card.Link>}
+                {dogPage &&
+                    <Container className={styles.DogDetails}>
+                        <Row>
+                            <Col className="py-2 p-0 p-lg-2" md={4}>
+                                <i className={`fa-solid fa-dog ${styles.Icon}`}/> {breed}
+                            </Col>
+                            <Col className="py-2 p-0 p-lg-2" md={4}>
+                                <i className={`fa-solid fa-weight-scale ${styles.Icon}`}/> {size}
+                            </Col>
+                            <Col className="py-2 p-0 p-lg-2" md={4}>
+                                <i className={`fa-solid fa-venus-mars ${styles.Icon}`}/> {gender}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="py-2 p-0 p-lg-2">
+                                <i className={`fa-solid fa-cake-candles ${styles.Icon}`}/> {birthday} ({age})
+                            </Col>
+                        </Row>
+                        {characteristics && characteristics.length > 0 &&
+                            <div className="mt-4 d-flex gap-3">
+                                {characteristics.map((char, index) => (
+                                    <div className={styles.Characteristic} key={index}>{char.characteristic}</div>
+                                ))}
+                            </div>
+                        }
+                    </Container>
+                }
             </Card.Body>
             <ConfirmationModal title="Confirm deletion" text="Are you sure you want to delete this dog?"
                 show={showConfirmation}
