bee454ee8b31503e90ea21f6e2cecfd3ccfb64c7
julia Wager <julia@p-wagner.at>
2024-06-01 15:15:00 +0200

    fix: add loading state

--- src/pages/dogs/DogEditForm.js
+++ src/pages/dogs/DogEditForm.js
@@ -26,6 +26,7 @@ function DogEditForm() {
 
     const navigate = useNavigate();
     const quillRef = useRef(null);
+    const [loading, setLoading] = useState(false);
 
     const handleGoBack = () => {
         navigate(-1);
@@ -205,6 +206,7 @@ function DogEditForm() {
 
     const handleSubmit = async (event) => {
         event.preventDefault()
+        setLoading(true);
         const formData = new FormData();
 
         formData.append("name", name)
@@ -243,6 +245,8 @@ function DogEditForm() {
             if (err.response?.status !== 401) {
                 setErrors(err.response?.data)
             }
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -377,11 +381,15 @@ function DogEditForm() {
 
                                 <div className="mt-4">
                                     <Button className={`${btnStyles.ReverseButton} ${btnStyles.Button}`}
-                                            onClick={handleGoBack}>
+                                            onClick={handleGoBack} disabled={loading}>
                                         Go back
                                     </Button>
-                                    <Button className={`${btnStyles.Button}`} type="submit">
-                                        Save Dog
+                                    <Button className={`${btnStyles.Button}`} type="submit" disabled={loading}>
+                                        {loading ? (
+                                            <Spinner animation="border" size="sm" />
+                                        ) : (
+                                            "Save Dog"
+                                        )}
                                     </Button>
                                 </div>
                             </div>
